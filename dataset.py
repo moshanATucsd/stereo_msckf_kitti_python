169445a1d46723a9d6497126d57badea9686157d
moshan <moshan@ucsd.edu>
2020-05-25 11:31:16 -0700

    publisher works

--- dataset.py
+++ dataset.py
@@ -2,88 +2,74 @@ import numpy as np
 import cv2
 import os
 import time
-
+import pandas as pd
 from collections import defaultdict, namedtuple
-
 from threading import Thread
 
-
+import utils 
 
 class GroundTruthReader(object):
-    def __init__(self, path, scaler, starttime=-float('inf')):
-        self.scaler = scaler   # convert timestamp from ns to second
-        self.path = path
+    def __init__(self, path, starttime=-float('inf')):
+        self.df = pd.read_pickle(path)
         self.starttime = starttime
-        self.field = namedtuple('gt_msg', ['p', 'q', 'v', 'bw', 'ba'])
+        self.field = namedtuple('gt_msg', ['timestamp', 'p', 'q', 'v'])
 
-    def parse(self, line):
+    def parse(self, i):
         """
-        line: (timestamp, p_RS_R_x [m], p_RS_R_y [m], p_RS_R_z [m], 
-        q_RS_w [], q_RS_x [], q_RS_y [], q_RS_z [], 
-        v_RS_R_x [m s^-1], v_RS_R_y [m s^-1], v_RS_R_z [m s^-1], 
-        b_w_RS_S_x [rad s^-1], b_w_RS_S_y [rad s^-1], b_w_RS_S_z [rad s^-1], 
-        b_a_RS_S_x [m s^-2], b_a_RS_S_y [m s^-2], b_a_RS_S_z [m s^-2])
         """
-        line = [float(_) for _ in line.strip().split(',')]
-
-        timestamp = line[0] * self.scaler
-        p = np.array(line[1:4])
-        q = np.array(line[4:8])
-        v = np.array(line[8:11])
-        bw = np.array(line[11:14])
-        ba = np.array(line[14:17])
-        return self.field(timestamp, p, q, v, bw, ba)
+        timestamp = self.df.loc[i, "timestamp"]
+        T_gt = self.df.loc[i, "T_i_vk"]
+        p = T_gt[:3, 3]
+        q = utils.to_quaternion(T_gt[:3, :3])
+        v = self.df.loc[i, "v_vk_i_vk"]
+        return self.field(timestamp, p, q, v)
 
     def set_starttime(self, starttime):
         self.starttime = starttime
 
     def __iter__(self):
-        with open(self.path, 'r') as f:
-            next(f)
-            for line in f:
-                data = self.parse(line)
-                if data.timestamp < self.starttime:
-                    continue
-                yield data
+        for i in range(len(self.df)):
+            data = self.parse(i)
+            if data.timestamp < self.starttime:
+                continue
+            yield data
 
+    def get_poses(self):
+        return np.array(list(self.df.loc[:, "T_i_vk"].values))
 
+    def get_velocities(self):
+        return np.array(list(self.df.loc[:, "v_vk_i_vk"]))
+
+    def get_timestamps(self):
+        return np.array(list(self.df.loc[:, "timestamp"]))
 
 class IMUDataReader(object):
-    def __init__(self, path, scaler, starttime=-float('inf')):
-        self.scaler = scaler
-        self.path = path
-        self.starttime = starttime
+    def __init__(self, path):
+        self.df = pd.read_pickle(path)
+        self.starttime = -float('inf')
         self.field = namedtuple('imu_msg', 
             ['timestamp', 'angular_velocity', 'linear_acceleration'])
 
-    def parse(self, line):
+    def parse(self, i, j):
         """
-        line: (timestamp [ns],
-        w_RS_S_x [rad s^-1], w_RS_S_y [rad s^-1], w_RS_S_z [rad s^-1],  
-        a_RS_S_x [m s^-2], a_RS_S_y [m s^-2], a_RS_S_z [m s^-2])
         """
-        line = [float(_) for _ in line.strip().split(',')]
-
-        timestamp = line[0] * self.scaler
-        wm = np.array(line[1:4])
-        am = np.array(line[4:7])
+        timestamp = self.df.loc[i, "imu_timestamps"][j]
+        wm = self.df.loc[i, "accel_measurements"][j]
+        am = self.df.loc[i, "gyro_measurements"][j]
+        # print("imu timestamps {}".format(timestamp))
         return self.field(timestamp, wm, am)
 
     def __iter__(self):
-        with open(self.path, 'r') as f:
-            next(f)
-            for line in f:
-                data = self.parse(line)
+        for i in range(len(self.df)):
+            for j in range(len(self.df.loc[i, "imu_timestamps"])):
+                data = self.parse(i, j)
                 if data.timestamp < self.starttime:
                     continue
                 yield data
 
     def start_time(self):
-        # return next(self).timestamp
-        with open(self.path, 'r') as f:
-            next(f)
-            for line in f:
-                return self.parse(line).timestamp
+        first_start_time = self.df.loc[0, "imu_timestamps"][0]
+        return first_start_time
 
     def set_starttime(self, starttime):
         self.starttime = starttime
@@ -91,10 +77,17 @@ class IMUDataReader(object):
 
 
 class ImageReader(object):
-    def __init__(self, ids, timestamps, starttime=-float('inf')):
-        self.ids = ids
+    def __init__(self, img_path, data_path):
+        self.ids = self.list_imgs(img_path)
+        self.df = pd.read_pickle(data_path)
+
+        timestamps = []
+        for i in range(len(self.df)):
+            timestamps.append(self.df.loc[i, "timestamp"])
+
         self.timestamps = timestamps
-        self.starttime = starttime
+        self.starttime = self.start_time()
+
         self.cache = dict()
         self.idx = 0
 
@@ -114,6 +107,7 @@ class ImageReader(object):
         t = float('inf')
         while True:
             if time.time() - t > self.wait:
+                # print("insufficient time for imread")
                 return
             if self.idx == idx:
                 time.sleep(1e-2)
@@ -141,22 +135,31 @@ class ImageReader(object):
         if idx in self.cache:
             img = self.cache[idx]
             del self.cache[idx]
+            # print("image read cache")
         else:   
             img = self.read(self.ids[idx])
+            # print("image read")
         return img
 
     def __iter__(self):
         for i, timestamp in enumerate(self.timestamps):
             if timestamp < self.starttime:
+                print("image continue")
                 continue
+            # print("image iter {}".format(i))
             yield self.field(timestamp, self[i])
 
     def start_time(self):
-        return self.timestamps[0]
+        first_start_time = self.timestamps[0]
+        return first_start_time
 
     def set_starttime(self, starttime):
         self.starttime = starttime
 
+    def list_imgs(self, dir):
+        xs = [_ for _ in os.listdir(dir) if _.endswith('.png')]
+        xs = sorted(xs, key=lambda x:float(x[:-4]))
+        return [os.path.join(dir, _) for _ in xs]
 
 
 class Stereo(object):
@@ -191,24 +194,18 @@ class kittiDataset(object):   # Stereo + IMU
     '''
     path example: '/media/erl/disk2/kitti/2011_09_30/2011_09_30_drive_0027_extract'
     '''
-    def __init__(self, path):
-        self.groundtruth = GroundTruthReader(os.path.join(
-            path, 'mav0', 'state_groundtruth_estimate0', 'data.csv'), 1e-9)
-        self.imu = IMUDataReader(os.path.join(
-            path, 'mav0', 'imu0', 'data.csv'), 1e-9)
-        self.cam0 = ImageReader(
-            *self.list_imgs(os.path.join(path, 'image_02', 'data')))
-        self.cam1 = ImageReader(
-            *self.list_imgs(os.path.join(path, 'image_03', 'data')))
+    def __init__(self, img_path, data_path):
+        self.groundtruth = GroundTruthReader(os.path.join(data_path, 'data.pickle'))
+        self.imu = IMUDataReader(os.path.join(data_path, 'data.pickle'))
+
+        self.cam0 = ImageReader(os.path.join(img_path, 'image_02', 'data'), os.path.join(data_path, 'data.pickle'))
+        self.cam1 = ImageReader(os.path.join(img_path, 'image_03', 'data'), os.path.join(data_path, 'data.pickle'))
 
         self.stereo = Stereo(self.cam0, self.cam1)
         self.timestamps = self.cam0.timestamps
 
-        # self.starttime = max(self.imu.start_time(), self.stereo.start_time())
-        # self.set_starttime(0)
-
-        self.starttime = 0
-        self.stereo.set_starttime(0)
+        self.starttime = max(self.imu.start_time(), self.stereo.start_time())
+        self.set_starttime(0)
 
     def set_starttime(self, offset):
         self.groundtruth.set_starttime(self.starttime + offset)
@@ -216,13 +213,6 @@ class kittiDataset(object):   # Stereo + IMU
         self.cam0.set_starttime(self.starttime + offset)
         self.cam1.set_starttime(self.starttime + offset)
         self.stereo.set_starttime(self.starttime + offset)
-
-    def list_imgs(self, dir):
-        xs = [_ for _ in os.listdir(dir) if _.endswith('.png')]
-        xs = sorted(xs, key=lambda x:float(x[:-4]))
-        timestamps = [float(_[:-4]) * 1e-9 for _ in xs]
-        return [os.path.join(dir, _) for _ in xs], timestamps
-
 
 
 # simulate the online environment
@@ -267,66 +257,71 @@ class DataPublisher(object):
                 if self.stopped:
                     return
 
-            if interval <= self.duration + 1e-3:
-                self.out_queue.put(data)
-            else:
-                self.out_queue.put(None)
-                return
+            # if interval <= self.duration + 1e-3:
+            #     self.out_queue.put(data)
+            # else:
+            #     print("exception")
+            #     self.out_queue.put(None)
+            #     return
+            self.out_queue.put(data)
 
-
+def print_msg(in_queue, source):
+    while True:
+        x = in_queue.get()
+        if x is None:
+            return
+        print(x.timestamp, source)
 
 if __name__ == '__main__':
     from queue import Queue
 
-    path = '/media/erl/disk2/kitti/2011_09_30/2011_09_30_drive_0027_extract'
-    dataset = kittiDataset(path)
+    img_path = '/media/erl/disk2/kitti/2011_09_30/2011_09_30_drive_0027_extract'
+    data_path = '/home/erl/Workspace/deep_ekf_vio/data/K07'
+    dataset = kittiDataset(img_path, data_path)
     dataset.set_starttime(offset=0)
 
+    imu_queue = Queue()
     img_queue = Queue()
-    # imu_queue = Queue()
     # gt_queue = Queue()
 
     duration = 1
-    # imu_publisher = DataPublisher(
-    #     dataset.imu, imu_queue, duration)
+
+    imu_publisher = DataPublisher(
+        dataset.imu, imu_queue, duration)
     img_publisher = DataPublisher(
         dataset.stereo, img_queue, duration)
     # gt_publisher = DataPublisher(
     #     dataset.groundtruth, gt_queue, duration)
 
     now = time.time()
-    # imu_publisher.start(now)
+    imu_publisher.start(now)
     img_publisher.start(now)
     # gt_publisher.start(now)
 
-    def print_msg(in_queue, source):
-        while True:
-            x = in_queue.get()
-            if x is None:
-                return
-            print(x.timestamp, source)
-    # t2 = Thread(target=print_msg, args=(imu_queue, 'imu'))
+    t2 = Thread(target=print_msg, args=(imu_queue, 'imu'))
     # t3 = Thread(target=print_msg, args=(gt_queue, 'groundtruth'))
-    # t2.start()
+
+    t2.start()
     # t3.start()
 
     timestamps = []
     while True:
         x = img_queue.get()
         if x is None:
+            print("no image")
             break
         print(x.timestamp, 'image')
-        cv2.imshow('left', np.hstack([x.cam0_image, x.cam1_image]))
-        cv2.waitKey(1)
+        # cv2.imshow('left', np.hstack([x.cam0_image, x.cam1_image]))
+        # cv2.waitKey(1)
         timestamps.append(x.timestamp)
 
-    # imu_publisher.stop()
+    imu_publisher.stop()
     img_publisher.stop()
     # gt_publisher.stop()
-    # t2.join()
+
+    t2.join()
     # t3.join()
 
-    print(f'\nelapsed time: {time.time() - now}s')
-    print(f'dataset time interval: {timestamps[-1]} -> {timestamps[0]}'
-        f'  ({timestamps[-1]-timestamps[0]}s)\n')
-    print('Please check if IMU and image are synced')
+    # print("elapsed time: {} s".format(time.time() - now))
+    # print("dataset time interval: {} -> {}, {} s".format(timestamps[-1], timestamps[0], timestamps[-1]-timestamps[0]))
+    # print('Please check if IMU and image are synced')
